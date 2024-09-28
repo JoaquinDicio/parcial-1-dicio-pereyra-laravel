4e83874ff5d6ba7fd21c346fcc0d674f27b7c753
joacodicio <joacodicio@gmail.com>
2024-09-28 15:29:26 -0300

    se puede registrar un user con el rol correcto

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -13,7 +13,7 @@ class userController extends Controller
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->password = ($request->input('password'));
-        $user->role_id = 1;
+        $user->role_id = 3;
 
         $user->save(); // guarda todito
     }
