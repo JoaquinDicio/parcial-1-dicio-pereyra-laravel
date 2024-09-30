e0291b95759449ef6e6275e52d9c266d487334ed
Nico :D <nicopereyra855@gmail.com>
2024-09-29 22:35:47 -0300

    arreglo de bugs

--- database/migrations/2024_09_28_165617_create_services_table.php
+++ database/migrations/2024_09_28_165617_create_services_table.php
@@ -3,9 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,9 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->string("description");
-            $table->decimal("price",10,2); // para que tenga comas asi mas como moneda
+            $table->decimal("price", 10, 2); // para que tenga comas asi mas como moneda
             $table->timestamps();
         });
+
+        DB::table('services')->insert([
+            ['name' => 'Host re piola', 'description' => 'Descripcion de Host re piola', 'price' => '30000', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Host re malo', 'description' => 'Descripcion de Host re malo pero re mal, una cagada', 'price' => '10000', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     /**
