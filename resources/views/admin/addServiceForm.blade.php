4c67dcc691c3e400043ddf05ae3f2590d5fd66f9
Nico :D <nicopereyra855@gmail.com>
2024-10-01 15:20:25 -0300

    2

--- resources/views/admin/addServiceForm.blade.php
+++ resources/views/admin/addServiceForm.blade.php
@@ -9,7 +9,7 @@
 
         <div class="mb-4">
             <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nombre:</label>
-            <input type="text" id="name" name="name" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Ingrese el nombre del servicio">
+            <input type="text" id="name" name="name" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Ingrese el nombre del servicio" value={{ old('name') }}>
             @if ($errors->has('name'))
                 <span class="text-red-500 text-xs italic">{{ $errors->first('name') }}</span>
             @endif
@@ -17,7 +17,7 @@
         
         <div class="mb-4">
             <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Descripción:</label>
-            <textarea id="description" name="description" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Ingrese la descripción del servicio"></textarea>
+            <textarea id="description" name="description" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Ingrese la descripción del servicio">{{ old('description') }}</textarea>
             @if ($errors->has('description'))
                 <span class="text-red-500 text-xs italic">{{ $errors->first('description') }}</span>
             @endif
@@ -25,14 +25,14 @@
 
         <div class="mb-4">
             <label for="price" class="block text-gray-700 text-sm font-bold mb-2">Precio:</label>
-            <input type="number" id="price" name="price" step="0.01" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Ingrese el precio del servicio">
+            <input type="number" id="price" name="price" step="0.01" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Ingrese el precio del servicio" value={{ old('price') }}>
             @if ($errors->has('price'))
                 <span class="text-red-500 text-xs italic">{{ $errors->first('price') }}</span>
             @endif
         </div>
 
         <div class="flex items-center justify-between">
-            <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-medium text-sm py-2 px-3 rounded" value="Guardar"/>
+            <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-medium text-sm py-2 px-3 rounded cursor-pointer" value="Guardar"/>
         </div>
     </form>
 </div>
